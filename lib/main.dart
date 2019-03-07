e5e69bd98e09db267e0f0c9ead11f02bec416b7c
Labohkip81 <Labankiplagat81@gmail.com>
2019-03-08 00:15:06 +0300

    Created the Entry Page for the app;

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 // This is the entry point for our application
 import "package:flutter/material.dart";
-import 'package:pa';
+import "dashboard/dashboard.dart";
+
+void main() => runApp( new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  //This widget is the root of the application
+
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: 'K-Dharura',
+      theme: new ThemeData(
+
+        primarySwatch: Colors.grey,
+
+      ),
+
+      home: Dashboard(),
+    );
+  }
+}
