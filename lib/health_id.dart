0f6f51c14291da385320805341a78734ac83d339
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-17 12:41:09 +0300

    building the health Id

--- lib/health_id.dart
+++ lib/health_id.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-void main()=>runApp(HealthId());
+
+
+void main() => runApp(new HealthId());
 
 
 class HealthId extends StatefulWidget {
@@ -11,8 +12,31 @@ class HealthId extends StatefulWidget {
 class _HealthIdState extends State<HealthId> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            icon:Icon(Icons.arrow_back),
+            onPressed: (){Navigator.pop(context);},),
+          title: Text('Health ID',),
+          actions: <Widget>[
+            IconButton(
+              icon:new Icon(Icons.save),
+              onPressed: (){},)
+          ],
+        ),
+        body: Center(
+          child:Container(
+            color: Colors.blueAccent,
+          )),
+      ),
+      
       
     );
   }
 }
+
+
+ 
