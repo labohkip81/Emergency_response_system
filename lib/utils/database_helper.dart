80e0eb9105231b63ff85e490b3ac607a3fef6d7d
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-18 22:35:46 +0300

    database helper done

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -48,15 +48,38 @@ class DatabaseHelper{
     await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT,$colBlood TEXT,$colHealth TEXT, $colDoctor TEXT, $colPriority TEXT)');
   }
   // Fetch operation: Insert all note objects from database
-  getNoteMpaList()async{
+  Future<List<Map<String,dynamic>>>getNoteMpaList()async{
     Database db =await this.database;
-    var result =await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    // var result =await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
     var result =await db.query(noteTable, orderBy: '$colPriority ASC');
+    return result;
   }
   // Insert operation: Insert a Note object to database
+  Future<int> insertNote(Note note) async{
+    Database db =await this.database;
+    var result = await db.insert(noteTable, note.toMap());
+    return  result;
+
+
+  }
   // Update Operations: Update a Note object and save it to database
+  Future <int> updateNote(Note note) async{
+    var db =await this.database;
+    var result =await db.update(noteTable, note.toMap(),where: '$colId = ?', whereArgs:[note.id]);
+    return result;
+  }
   // Delete operation : Delete a Note object from database
+   Future <int> deleteNote(int id) async{
+    var db =await this.database;
+    int result =await db.rawDelete('DELET FROM $noteTable WHERE $colId = $id ');
+    return result;
   // Get number of he nte objects in the database
+  Future <int> getCount() async{
+    Database db =await this.database;
+    List<Map<String, dynamic>>x =await db.rawQuery('SELECT COUNT (*) from $noteTable');
+    int result =Sqflite.firstIntValue(x);
+    return result;
+  }
 
 
 }
