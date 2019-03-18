b806b93a8149d3bd1cd9c22c97a9c00c8f8c33e4
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-18 10:43:55 +0300

    scheming nearly done

--- lib/health_id.dart
+++ lib/health_id.dart
@@ -15,6 +15,7 @@ class _HealthIdState extends State<HealthId> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomPadding: false,
         backgroundColor: Colors.white,
         appBar: AppBar(
           leading: IconButton(
@@ -22,18 +23,20 @@ class _HealthIdState extends State<HealthId> {
             onPressed: (){Navigator.pop(context);},),
           title: Text('Health ID',),
           actions: <Widget>[
-            IconButton(
-              icon:new Icon(Icons.save),
-              onPressed: (){},)
-          ],
+            RaisedButton(
+              onPressed: (){},
+              child: Text('SAVE'),
+            ),
+             ],
         ),
         body: Center(
           
-          child:Column(
+          child:ListView(
             
          children: <Widget>[
-           Padding(padding: EdgeInsets.only(top: 50.0),),
+           Padding(padding: EdgeInsets.only(top: 30.0),),
            Container(
+             padding: EdgeInsets.only(left: 130,right: 130),
              child: Text('Profile Picture'),
 
            ),
@@ -58,8 +61,46 @@ class _HealthIdState extends State<HealthId> {
                
              ),
              child: ClipOval(child:Icon(Icons.account_circle),),
-             
            ),
+           SizedBox(height: 20.0,),
+           Container(
+             padding: EdgeInsets.all(20.0),
+             child: Text('Fill the fields below'),
+           ),
+           Container(
+             padding: EdgeInsets.all(20.0),
+             child: TextFormField(decoration: InputDecoration(
+               border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+               hintText: 'A, B, AB, 0',
+               labelText: 'Blood Group',
+               labelStyle: TextStyle(
+                 fontSize: 15.0,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),),
+           ),
+           Container(
+             padding: EdgeInsets.all(20.0),
+             child: TextFormField(
+               style: Theme.of(context).textTheme.title,
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                 labelText: 'Health Complications',
+                 hintText: 'e.g Liver disease, Diabetes ',
+                 labelStyle: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)
+             ),),
+           ),
+           Container(
+             padding: EdgeInsets.all(20.0),
+             child: TextFormField(
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                 labelStyle: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                 labelText: 'Doctor`s note',
+                 hintText: 'What should be done when you in a certain condition',
+               ),
+             ),
+           )
            
          ],
             
