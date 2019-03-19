330774eecd6fbe7ebe6a2067a001220c70c86cd4
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-19 12:13:36 +0300

    abastract class created

---
+++ lib/pager/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:kdharura/data/rest_data.dart';
+import 'package:kdharura/model/user.dart';
+
+abstract class LoginPageContract{
+  void onLoginSuccess(User user);
+  void onLoginError(String error);
+
+}
+class  LoginPagePresenter {
+  LoginPageContract _view;
+  RestData api = new RestData();
+  LoginPagePresenter(this._view);
+
+  doLogin(String username, String password ){
+    api.login(username, password).then((user)=>_view.onLoginSuccess(user)).catchError((onError)=>_view.onLoginError(onError()));
+  }
+}
